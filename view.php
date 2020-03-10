814f87bd83392eb64e1085b832ef55694a468948
工藤海里 <49087795+kudokairi@users.noreply.github.com>
2020-03-11 01:42:26 +0900

    つぶやき一覧画面のレイアウトと、つぶやき詳細画面のレイアウトと表示ロジックを変更しました。（選択つぶやきに対するコメントも表示するように）

--- view.php
+++ view.php
@@ -7,8 +7,8 @@ if(empty($_REQUEST['id'])){
     exit();
 }
 
-$posts = $db->prepare('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id AND p.id=?');
-$posts->execute(array($_REQUEST['id']));
+$posts = $db->prepare('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id AND p.id=? UNION SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id AND p.reply_message_id=?');
+$posts->execute(array($_REQUEST['id'],$_REQUEST['id']));
 
 ?>
 
@@ -31,13 +31,18 @@ $posts->execute(array($_REQUEST['id']));
   </div>
   <div id="content">
   <p>&laquo;<a href="index.php">一覧にもどる</a></p>
-  <?php if($post = $posts->fetch()): ?>
-    <div class="msg">
-    <img src="member_picture/<?php print(htmlspecialchars($post['picture'])); ?>"width='300px' height='auto'> 
-    <p><?php print(htmlspecialchars($post['message'])); ?><span class="name">（<?php print(htmlspecialchars($post['name'])); ?>）</span></p>
-    <p class="day"><?php print(htmlspecialchars($post['created'])); ?></p>
-    </div>
-  <?php else: ?>
+  <?php foreach($posts as $posted): ?>
+        <div class="msg">
+            <div class="message_img">
+                <img src="member_picture/<?php print(htmlspecialchars($posted['picture'])); ?>"width='300px' height='auto'> 
+            </div>
+            <div class="message_text">
+                <p><span class="name"><?php print(htmlspecialchars($posted['name'])); ?></span>　<span class="day"><?php print(htmlspecialchars($posted['created'])); ?></span></p>
+                <p><?php print(htmlspecialchars($posted['message'])); ?></p>
+            </div>
+        </div>
+    <?php endforeach; ?>
+    <?php if(empty($posted)): ?>
 	<p>その投稿は削除されたか、URLが間違えています</p>
   <?php endif; ?>
 </div>
